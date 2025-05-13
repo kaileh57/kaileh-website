ad3e002009ae258b8a07ea125741467c4c736bae
kaileh57 <kellenhe@hotmail.com>
2025-05-12 20:12:23 -0700

    .

--- chem/src/state/store.js
+++ chem/src/state/store.js
@@ -1,6 +1,12 @@
 // Redux Toolkit store configuration will go here
-// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import gameReducer from './gameSlice';
 
-// export const store = configureStore({
-//   reducer: {},
-// }); 
+export const store = configureStore({
+  reducer: {
+    game: gameReducer,
+    // Add other reducers here as needed (e.g., map, turn, ui)
+  },
+});
+
+export default store; 
