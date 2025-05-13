0cdecc04fc124539025b7260d3dc4a7f7fe0ce73
kaileh57 <kellenhe@hotmail.com>
2025-05-13 10:06:48 -0700

    additional reversion

--- chem/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Electrifying the USA - Coming Soon!</h1>
-    </div>
-  );
-}
-
-export default App; 
