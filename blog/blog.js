a364417a35edb75fd5bb96f8c8b4cebe39027e6a
kaileh57 <kellenhe@hotmail.com>
2026-02-12 21:12:07 -0800

    SEO and cards ect

--- blog/blog.js
+++ blog/blog.js
@@ -1,15 +1,15 @@
 // Simple, functional JavaScript for the blog
-document.addEventListener('DOMContentLoaded', function() {
-    
+document.addEventListener('DOMContentLoaded', function () {
+
     // Add some basic functionality without being fancy
     addKeyboardShortcuts();
     improveLinks();
-    
+
 });
 
 // Basic keyboard shortcuts
 function addKeyboardShortcuts() {
-    document.addEventListener('keydown', function(e) {
+    document.addEventListener('keydown', function (e) {
         // Press 'h' to go home
         if (e.key === 'h' && !e.ctrlKey && !e.metaKey && !isTyping()) {
             window.location.href = '../index.html';
@@ -52,7 +52,7 @@ function generateMetadata(postData) {
     } = postData;
 
     const keywords = tags.concat(['AI', 'artificial intelligence', 'technology', 'blog']).join(', ');
-    
+
     return `    <!-- SEO Meta Tags -->
     <meta name="description" content="${description}">
     <meta name="keywords" content="${keywords}">
@@ -62,8 +62,8 @@ function generateMetadata(postData) {
     <meta property="og:title" content="${title}">
     <meta property="og:description" content="${description}">
     <meta property="og:type" content="article">
-    <meta property="og:url" content="https://kaileh57.com/blog/posts/${url}">
-    <meta property="og:site_name" content="Kaileh57">
+    <meta property="og:url" content="https://kaileh.dev/blog/posts/${url}">
+    <meta property="og:site_name" content="Kellen Heraty">
     <meta property="og:locale" content="en_US">
     <meta property="article:published_time" content="${publishedDate}">
     <meta property="article:author" content="${author}">
